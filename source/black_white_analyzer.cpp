ef5ae523ed8feb3703052a85b75264888ae9a4d6
Bruno Rodrigues <brunounbgama@gmail.com>
2015-10-21 13:54:57 +0000

    Ajusts at matrix displacement recognizer

--- source/black_white_analyzer.cpp
+++ source/black_white_analyzer.cpp
@@ -2,7 +2,7 @@
 #include "black_white_analyzer.hpp"
 #include <vector>
 
-#define PIXELS_ERROR 10
+#define PIXELS_ERROR 150
 #define MATRIX_DISPLACEMENT_ERROR 20
 
 
@@ -42,17 +42,17 @@ BlackWhiteAnalyzer::instruction BlackWhiteAnalyzer::generateCommand(vector<vecto
 					i++;
 			}
 
-      if( (j == matrix[0].size()-2) and (collumn_of_reference == -1) )
-      {
-        j = 0;
-        i++;
-      }
+		      if( (j == matrix[0].size()-2) and (collumn_of_reference == -1) )  {
+        	      	j = 0;
+     			i++;
+     	              }
 		}
 
 		for(unsigned int j = 0 ; j < distance_from_column_of_reference.size(); j++) {
 			x += distance_from_column_of_reference[j];
 		}
-		// cout<<"X: "<<x<<endl;
+
+
 
 		if (x < -PIXELS_ERROR){
 			return BlackWhiteAnalyzer::go_left;
@@ -72,12 +72,15 @@ BlackWhiteAnalyzer::instruction BlackWhiteAnalyzer::generateCommand(vector<vecto
 
 BlackWhiteAnalyzer::instruction BlackWhiteAnalyzer::verifyCenterMatrix(vector<vector<int> > matrix) {
 	
-	int number_of_collumns = matrix[0].size();
-	int count_before_line = 0;
-	int count_after_line = 0;
+	unsigned int number_of_collumns = matrix[0].size();
+	unsigned int number_of_lines = matrix.size();
+	unsigned int count_before_line = 0;
+	unsigned int count_after_line = 0;
 	bool before_the_middle = true;
+	unsigned int i = 0;
 
 	for(unsigned int j=0; j<number_of_collumns - 1; j++) {
+		
 
 		if(before_the_middle)
 			count_before_line++;
@@ -86,16 +89,29 @@ BlackWhiteAnalyzer::instruction BlackWhiteAnalyzer::verifyCenterMatrix(vector<ve
 			count_after_line++;
 		}
 
-		if(matrix[0][j+1] == 255 ){
+		if(matrix[0][j+1] != 0 ){
 			before_the_middle = false;
 		} 
+
+		if (j == number_of_collumns - 2 && i < number_of_lines - 1) {
+			i++;
+			j = 0;
+		}
 	}
 
 	float x = count_before_line - count_after_line;
+//	cout<<"Before Line: "<<count_before_line<<endl;
+//	cout<<"After Line: "<<count_after_line<<endl;
+//	cout<<"X: "<<x<<endl;
 
 	if (x < -MATRIX_DISPLACEMENT_ERROR){
 			return BlackWhiteAnalyzer::go_left;
 	}
+	
+	else if (count_after_line == 0) {
+		return BlackWhiteAnalyzer::go_foward;
+	}
+
 	else if (x == 0 || (x > -MATRIX_DISPLACEMENT_ERROR && x < MATRIX_DISPLACEMENT_ERROR)) {
 		return BlackWhiteAnalyzer::go_foward;
 	}
