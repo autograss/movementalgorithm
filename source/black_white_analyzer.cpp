00e74a6735e6abe2d21609ad4b07f958efaf49c1
David Carlos <ddavidcarlos1392@gmail.com>
2015-11-11 14:16:00 -0200

    Fix black white analyzer

--- source/black_white_analyzer.cpp
+++ source/black_white_analyzer.cpp
@@ -2,129 +2,85 @@
 #include "black_white_analyzer.hpp"
 #include <vector>
 
-#define PIXELS_ERROR 150
-#define MATRIX_DISPLACEMENT_ERROR 20
-
+#define PIXELS_ERROR 10
 
 using namespace std;
 
 
 BlackWhiteAnalyzer::instruction BlackWhiteAnalyzer::analyzeMatrixOfPixels (vector<vector<int> > matrix) {
 
-	BlackWhiteAnalyzer::instruction result = generateCommand(matrix);
+  int topLeft=-1, topRight=-1, bottomLeft=-1, bottomRight=-1;
+  int x;
 
-	if(result == go_foward){
-		result =  verifyCenterMatrix(matrix);
-	}
-	
-	return result;
-}
+  for(unsigned int i = 0; i < matrix.size(); i++)
+  {
+    for(unsigned int j = 0; j < matrix[i].size(); j++)
+    {
+      if(topLeft == -1)
+      {
+        if(matrix[i][j] == 255)
+          topLeft = j;
+      }
+      else if(topRight == -1)
+      {
+        if(matrix[i][j] == 0)
+          topRight = j-1;
+      }
+      else
+      {
+        break;
+      }
+    }
+  }
+  for(int i = matrix.size()-1; i >= 0; i--)
+  {
+    for(int j = matrix[i].size()-1; j >= 0; j--)
+    {
+      if(bottomRight == -1)
+      {
+        if(matrix[i][j] == 255)
+          bottomRight = j;
+      }
+      else if(bottomLeft == -1)
+      {
+        if(matrix[i][j] == 0)
+          bottomLeft = j-1;
+      }
+      else
+      {
+        break;
+      }
+    }
+  }
 
-BlackWhiteAnalyzer::instruction BlackWhiteAnalyzer::generateCommand(vector<vector<int> > matrix) {
+  x = ((topRight + topLeft) / 2) - ((bottomRight + bottomLeft) / 2);
 
-	vector<int> distance_from_column_of_reference;
-	vector<int> columns;
-	int x = 0;
-	unsigned int i = 0;
-	int collumn_of_reference = -1;
+  if (x < -PIXELS_ERROR){
+    return BlackWhiteAnalyzer::go_left;
+  }
+  else if (x == 0 || (x > -PIXELS_ERROR && x < PIXELS_ERROR)) {
+    return BlackWhiteAnalyzer::go_foward;
+  }
+  else if (x > PIXELS_ERROR) {
+    return BlackWhiteAnalyzer::go_right;
+  }
 
-		for(unsigned int j=0; j<matrix[0].size()-1; j++) {
-			if(matrix[i][j+1] == 255 ){
-				if(collumn_of_reference == -1) {
-					collumn_of_reference = j;
-				}
-
-				distance_from_column_of_reference.push_back(collumn_of_reference - j);
-				columns.push_back(j);
-				j=0;
-
-				if(i<matrix.size())
-					i++;
-			}
-
-		      if( (j == matrix[0].size()-2) and (collumn_of_reference == -1) )  {
-        	      	j = 0;
-     			i++;
-     	              }
-		}
-
-		for(unsigned int j = 0 ; j < distance_from_column_of_reference.size(); j++) {
-			x += distance_from_column_of_reference[j];
-		}
+	// x = columns[0];
 
 
+	// for(int i=0; i < columns.size(); i++){
 
-		if (x < -PIXELS_ERROR){
-			return BlackWhiteAnalyzer::go_left;
-		}
-		else if (x == 0 || (x > -PIXELS_ERROR && x < PIXELS_ERROR)) {
-			return BlackWhiteAnalyzer::go_foward;
-		}
-		else if (x > PIXELS_ERROR) {
-			return BlackWhiteAnalyzer::go_right;
-		}
+	// 	if(x != columns[i]) {
+	// 		cout<<"Pixels não alinhados!"<<endl;
+	// 		return BlackWhiteAnalyzer::go_left;
+	// 	}
 
-	return BlackWhiteAnalyzer::go_foward;
+	// }
 
-
-}
-
-
-BlackWhiteAnalyzer::instruction BlackWhiteAnalyzer::verifyCenterMatrix(vector<vector<int> > matrix) {
-	
-	unsigned int number_of_collumns = matrix[0].size();
-	unsigned int number_of_lines = matrix.size();
-	unsigned int count_before_line = 0;
-	unsigned int count_after_line = 0;
-	bool before_the_middle = true;
-	unsigned int i = 0;
-
-	for(unsigned int j=0; j<number_of_collumns - 1; j++) {
-		
-
-		if(before_the_middle)
-			count_before_line++;
-
-		if(before_the_middle == false && matrix[0][j] == 0) {
-			count_after_line++;
-		}
-
-		if(matrix[0][j+1] != 0 ){
-			before_the_middle = false;
-		} 
-
-		if (j == number_of_collumns - 2 && i < number_of_lines - 1) {
-			i++;
-			j = 0;
-		}
-	}
-
-	float x = count_before_line - count_after_line;
-//	cout<<"Before Line: "<<count_before_line<<endl;
-//	cout<<"After Line: "<<count_after_line<<endl;
-//	cout<<"X: "<<x<<endl;
-
-	if (x < -MATRIX_DISPLACEMENT_ERROR){
-			return BlackWhiteAnalyzer::go_left;
-	}
-	
-	else if (count_after_line == 0) {
-		return BlackWhiteAnalyzer::go_foward;
-	}
-
-	else if (x == 0 || (x > -MATRIX_DISPLACEMENT_ERROR && x < MATRIX_DISPLACEMENT_ERROR)) {
-		return BlackWhiteAnalyzer::go_foward;
-	}
-	else if (x > MATRIX_DISPLACEMENT_ERROR) {
-		return BlackWhiteAnalyzer::go_right;
-	}
+	// cout<<"Pixels alinhados!"<<endl;
 
 	return BlackWhiteAnalyzer::go_foward;
 }
-
-
-
-
 
 BlackWhiteAnalyzer::instruction
 BlackWhiteAnalyzer::getInstruction (Image image, unsigned int pixelsToBinary,
