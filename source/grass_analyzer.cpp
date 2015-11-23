f2aa220d87dbb6e551b4b2f815d61479cb7e469a
David Carlos <ddavidcarlos1392@gmail.com>
2015-11-23 20:10:05 +0000

    Binarize image with threshold function

--- source/grass_analyzer.cpp
+++ source/grass_analyzer.cpp
@@ -22,6 +22,9 @@ cv::Mat GrassAnalyzer::find_line(Image image)
     cv::Mat bin_image;
     threshold(blurred_image, bin_image, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
 
-    return bin_image;
+    cv::Mat final_image;
+    threshold(bin_image, final_image, 0, 255, cv::THRESH_BINARY);
+
+    return final_image;
 
 }
